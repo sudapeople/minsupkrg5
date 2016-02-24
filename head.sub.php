23fc828de4ce7ff82a6c70719d0279fd00e3261a
whitedot <whitedot@naver.com>
2016-02-24 23:35:26 +0900

    g5 5.1.13 update

--- head.sub.php
+++ head.sub.php
@@ -1,4 +1,6 @@
 <?php
+// 5.1.13
+
 // 이 파일은 새로운 파일 생성시 반드시 포함되어야 함
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
@@ -74,11 +76,9 @@ var g5_bo_table  = "<?php echo isset($bo_table)?$bo_table:''; ?>";
 var g5_sca       = "<?php echo isset($sca)?$sca:''; ?>";
 var g5_editor    = "<?php echo ($config['cf_editor'] && $board['bo_use_dhtml_editor'])?$config['cf_editor']:''; ?>";
 var g5_cookie_domain = "<?php echo G5_COOKIE_DOMAIN ?>";
-<?php
-if ($is_admin) {
-    echo 'var g5_admin_url = "'.G5_ADMIN_URL.'";'.PHP_EOL;
-}
-?>
+<?php if(defined('G5_IS_ADMIN')) { ?>
+var g5_admin_url = "<?php echo G5_ADMIN_URL; ?>";
+<?php } ?>
 </script>
 <script src="<?php echo G5_JS_URL ?>/jquery-1.8.3.min.js"></script>
 <script src="<?php echo G5_JS_URL ?>/jquery.menu.js"></script>
